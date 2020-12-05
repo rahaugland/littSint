30b423026fd403be741dba28e4063000b98b6354
Rasmus Longva Haugland <rasmusha@stud.ntnu.no>
2020-12-05 12:06:05 +0100

    build project with mvn in eclipse

---
+++ src/main/java/org/openjfx/littSint/App.java
@@ -0,0 +1,13 @@
+package org.openjfx.littSint;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
